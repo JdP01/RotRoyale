475ba9ea2cec80765d2174fdcfc02718e14270a9
JuanPleitez <juandipleitez04@gmail.com>
2025-09-15 12:36:57 -0600

    fixed vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@dimforge/rapier3d-compat']
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
 })
